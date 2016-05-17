84f4482fff51023581d69d847d8570730f3dfaf3
Manuel Reyes <yoggsoft@gmail.com>
2016-05-17 05:11:12 +0000

    Fixed > data parsing, XHR

--- src/sheetfinder.js
+++ src/sheetfinder.js
@@ -1,113 +1,87 @@
-var sheetfinder = (function () {
+;(function(window, document, undefined) {
     // use strict
     "use strict";
-
-    var _data = "";
-    var instance;
-    var events = [];
-    var event_keys = {
-      _init: "connected",
-      _keySave : "created",
-      _keyUpdate : "updated",
-      _keyDelete : "deleted",
-      _keyRead : "read"
-    };
-    var _key = '';
-
-    function createInstance() {
-        var _self = new Object();
-        return _self;
-    }
-
-    function _getInstance() {
-        if (!instance) {
-            instance = createInstance();
-        }
-        return instance;
-    }
-
-    function isValid (val) {
+    var NAME ="sheetfinder";
+    
+    function isValid(val) {
       var isString = typeof val === 'string';
       var notEmpty = val !== '';
       var notNull = val !== null;
-      return (isString && notEmpty && notNull) ? 1 : 0;
+      return (isString && notEmpty && notNull);
     }
-
-    function _connect(key) {
-      if (!isValid(key)) {
-          console.error('Invalid key.');
-      } else {
-        _key = key;
-        handleRequest();
+    
+    function sheetfinder(key){
+      this._key="";
+      this.url="";
+      this.events = [];
+      this.data={};
+      this.requester;
+      // this.event_keys = {
+      //   _init: "connected",
+      //   _keySave : "created",
+      //   _keyUpdate : "updated",
+      //   _keyDelete : "deleted",
+      //   _keyRead : "read"
+      // };
+      if(isValid(key)){
+          this._key = key;
+          this.url = 'https://spreadsheets.google.com/feeds/list/'+this._key+'/1/public/basic?alt=json';
+          // this.data = handleRequest(this.url);
+          this.requester = new requester(this.url);
+      }else{
+          console.error('Doc Key is invalid. Make sure that the document is published and the Key is valid');
       }
     }
-    // HandleRequest method - creates the Google Docs request
-    function handleRequest () {
-      var url = 'https://spreadsheets.google.com/feeds/list/'+_key+'/1/public/basic?alt=json';
-      var request = new XMLHttpRequest();
-      request.open('GET', url, true);
-      request.onload = function() {
-          if (request.status >= 200 && request.status < 400) {
-              // Success!
-              _data = JSON.parse(request.responseText);
-              dispatch (event_keys._init);
-              // return _data;
-          } else {
-              // We reached our target server, but it returned an error
-              console.error("Server returned an error. Check the spreadsheet key.");
-          }
-      };
-
-
-      // request.onerror = function() {
-      //   // Connection error of some sort.
-      //   console.error("Connection error.");
-      // };
-      request.send();
-    }
-
-    function dispatch (key) {
+    
+    sheetfinder.prototype.dispatch = function (key) {
   		var dataObj;
-  		if (events.hasOwnProperty(key)) {
+  		if (this.events.hasOwnProperty(key)) {
   			dataObj = dataObj || {};
   			dataObj.currentTarget = sheetfinder;
-  			for (var i in events[key]) {
-  				events[key][i](dataObj);
+  			for (var i in this.events[key]) {
+  				this.events[key][i](dataObj);
   			}
   		}
-  	}
-
-    // removes listeners
-    function _addEventListener (key,func) {
-  		if (!events.hasOwnProperty(key)) {
-  		    events[key] = [];
+  	};
+  	
+    sheetfinder.prototype.addEventListener = function (key,func) {
+  		if (!this.events.hasOwnProperty(key)) {
+  		    this.events[key] = [];
   		}
-  		events[key].push(func);
-  	}
+  		this.events[key].push(func);
+  	};
 
-  	// removes listeners
-  	function _removeEventListener (key,func) {
-  		if (events.hasOwnProperty(key)) {
-  			for (var i in events[key]) {
-  				if (events[key][i] === func) {
-  					events[key].splice(i, 1);
+  	sheetfinder.prototype.removeEventListener = function (key,func) {
+  		if (this.events.hasOwnProperty(key)) {
+  			for (var i in this.events[key]) {
+  				if (this.events[key][i] === func) {
+  					this.events[key].splice(i, 1);
   				}
   			}
   		}
-  	}
-
-    return {// Public API
-
-      getInstance : _getInstance,
-
-      // public methods
-      connect : _connect,
-      
-      // EventListener methods
-      addEventListener : _addEventListener,
-      removeEventListener : _removeEventListener,
-
-      connected : event_keys._init
-
+  	};
+  	
+  	var parsedata = function(data){
+  	  var spreadsheet = {};
+  	  spreadsheet.title = data.feed.title;
+  	  spreadsheet.author = data.feed.author;
+  	  spreadsheet.feed = data.feed;
+  	  console.log(spreadsheet);
+  	};
+  	// HandleRequest method - pulls data from 
+    var requester = function(url) {
+      var request = new XMLHttpRequest();
+      request.open('GET', url, true);
+      request.onreadystatechange = function(){
+        if (request.readyState === 4){
+          if (request.status >= 200 && request.status < 400) {
+            parsedata(JSON.parse(request.responseText));
+          }else{
+            console.error("Server returned an error. Check the spreadsheet key.");
+          }
+        }
+      };
+      request.send(null);
     }
-})();
+  window[NAME] = sheetfinder;
+})(window, document);
